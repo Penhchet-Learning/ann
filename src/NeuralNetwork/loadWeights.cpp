0120562c0d56dfbc977d1e3fd6d031141aa465c0
Raphael Alampay <raphael.alampay@gmail.com>
2017-05-14 17:57:06 +0800

    load weights if 'weights' config is present. Resolves #4

--- src/NeuralNetwork/loadWeights.cpp
+++ src/NeuralNetwork/loadWeights.cpp
@@ -2,8 +2,16 @@
 
 void NeuralNetwork::loadWeights(string filename) {
   std::ifstream i(filename);
-  json j;
-  i >> j;
+  json jWeights;
+  i >> jWeights;
 
-  vector< vector< vector<double> > > temp = j["weights"];
+  vector< vector< vector<double> > > temp = jWeights["weights"];
+
+  for(int i = 0; i < this->weightMatrices.size(); i++) {
+    for(int r = 0; r < this->weightMatrices.at(i)->getNumRows(); r++) {
+      for(int c = 0; c < this->weightMatrices.at(i)->getNumCols(); c++) {
+        this->weightMatrices.at(i)->setValue(r, c, temp.at(i).at(r).at(c));
+      }
+    }
+  }
 }
